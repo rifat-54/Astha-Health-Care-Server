28766c1a6279c99f1c1a203ba1d72d83222da1b9
rifat <rifat999por@gmail.com>
2026-07-18 21:52:43 +0600

    start astha health care development server

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import app from "./app";
+
+const main = () => {
+    try {
+        app.listen(5000, () => {
+            console.log(`Server is running on http://localhost:5000`);
+        });
+    } catch (error) {
+        console.error('Failed to start server:', error);
+    }
+}
+
+main();
