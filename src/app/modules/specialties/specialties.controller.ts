468fb30a5375921703321587c821ef3800347bf7
rifat <rifat999por@gmail.com>
2026-07-19 23:59:37 +0600

    added catch async fun

--- src/app/modules/specialties/specialties.controller.ts
+++ src/app/modules/specialties/specialties.controller.ts
@@ -1,38 +1,29 @@
 import { Request, Response } from "express";
 import { SpecialtyServices } from "./specialties.services";
+import { catchAsync } from "../../shared/catchAsync";
 
-const createSpecialty=async(req:Request,res:Response)=>{
-    try {
-        const payload=req.body
-        const result=await SpecialtyServices.createSpecialty(payload)
-        res.status(201).json({message:"Created",data:result})
-    } catch (error) {
-        
-    }
-}
+const createSpecialty = catchAsync(async (req: Request, res: Response) => {
+  const payload = req.body;
+  const result = await SpecialtyServices.createSpecialty(payload);
+  res.status(201).json({ message: "Created", data: result });
+});
 
-const getAllSpecialty=async(req:Request,res:Response)=>{
-    try {
-        const result =await SpecialtyServices.getAllSpecialty()
-        res.status(200).json({success:true,data:result})
-    } catch (error) {
-        
-    }
-}
+const getAllSpecialty = catchAsync(async (req: Request, res: Response) => {
+  const result = await SpecialtyServices.getAllSpecialty();
+  res.status(201).json({
+    success: true,
+    data: result,
+  });
+});
 
-const deleteSpecialty=async(req:Request,res:Response)=>{
-    try {
-        const id=req.params.id as string
-        const result=await SpecialtyServices.deleteSpecialty(id)
-        res.status(200).json({success:true,data:result})
-    } catch (error) {
-        
-    }
-}
+const deleteSpecialty = catchAsync(async (req: Request, res: Response) => {
+  const id = req.params.id as string;
+  const result = await SpecialtyServices.deleteSpecialty(id);
+  res.status(200).json({ success: true, data: result });
+});
 
-
-export const specialtyController={
-    createSpecialty,
-    getAllSpecialty,
-    deleteSpecialty
-}
+export const specialtyController = {
+  createSpecialty,
+  getAllSpecialty,
+  deleteSpecialty,
+};
