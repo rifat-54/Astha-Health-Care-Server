1338cf2708172b16df39b2a2a893e1bef83f8b3b
rifat <rifat999por@gmail.com>
2026-07-20 00:25:26 +0600

    add function sendResponse

--- src/app/modules/specialties/specialties.controller.ts
+++ src/app/modules/specialties/specialties.controller.ts
@@ -1,25 +1,38 @@
 import { Request, Response } from "express";
 import { SpecialtyServices } from "./specialties.services";
 import { catchAsync } from "../../shared/catchAsync";
+import { sendResponse } from "../../shared/sendResponse";
 
 const createSpecialty = catchAsync(async (req: Request, res: Response) => {
   const payload = req.body;
   const result = await SpecialtyServices.createSpecialty(payload);
-  res.status(201).json({ message: "Created", data: result });
+    sendResponse(res,{
+    httpStatusCode:201,
+    success:true,
+    message:"Created data",
+    data:result
+  })
 });
 
 const getAllSpecialty = catchAsync(async (req: Request, res: Response) => {
   const result = await SpecialtyServices.getAllSpecialty();
-  res.status(201).json({
-    success: true,
-    data: result,
-  });
+  sendResponse(res,{
+    httpStatusCode:200,
+    success:true,
+    message:"Feched data",
+    data:result
+  })
 });
 
 const deleteSpecialty = catchAsync(async (req: Request, res: Response) => {
   const id = req.params.id as string;
   const result = await SpecialtyServices.deleteSpecialty(id);
-  res.status(200).json({ success: true, data: result });
+   sendResponse(res,{
+    httpStatusCode:200,
+    success:true,
+    message:"Deleted data",
+    data:result
+  })
 });
 
 export const specialtyController = {
