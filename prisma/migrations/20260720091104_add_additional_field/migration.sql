f5408e8d01c78198d22a8c036873d5d9e166dce0
rifat <rifat999por@gmail.com>
2026-07-20 15:11:47 +0600

    add better auth addition field

---
+++ prisma/migrations/20260720091104_add_additional_field/migration.sql
@@ -0,0 +1,12 @@
+-- CreateEnum
+CREATE TYPE "UserRole" AS ENUM ('SUPER_ADMIN', 'ADMIN', 'DOCTOR', 'PATIENT');
+
+-- CreateEnum
+CREATE TYPE "UserStatus" AS ENUM ('BLOCKED', 'DELETED', 'ACTIVE');
+
+-- AlterTable
+ALTER TABLE "user" ADD COLUMN     "deletedAt" TIMESTAMP(3),
+ADD COLUMN     "isDeleted" BOOLEAN NOT NULL DEFAULT false,
+ADD COLUMN     "needPasswordChange" BOOLEAN NOT NULL DEFAULT false,
+ADD COLUMN     "role" TEXT NOT NULL DEFAULT 'PATIENT',
+ADD COLUMN     "status" TEXT NOT NULL DEFAULT 'ACTIVE';
