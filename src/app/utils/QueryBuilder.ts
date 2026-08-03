999bf05551c61ebf6769fbf9805525544bd6796d
rifat <rifat999por@gmail.com>
2026-08-04 01:37:51 +0600

    update query builder

--- src/app/utils/QueryBuilder.ts
+++ src/app/utils/QueryBuilder.ts
@@ -1,4 +1,4 @@
-import { PrismaCountArgs, PrismaFindManyArgs } from "../interface/query.interface";
+import { IQueryConfig, IQueryParams, PrismaCountArgs, PrismaFindManyArgs, PrismaModelDelegate, PrismaStringFilter, PrismaWhereConditions } from "../interface/query.interface";
 
 
 export class QueryBuilder<
@@ -15,5 +15,91 @@ TInclude=Record<string,unknown>
     private sortBy:string ="createdAt";
     private sortOrder:'asc'|'dsc'='dsc';
     private selectFields:Record<string,boolean> | undefined
+
+
+
+     constructor(
+        private model : PrismaModelDelegate,
+        private queryParams : IQueryParams,
+        private config : IQueryConfig = {}
+    ){
+        this.query = {
+            where : {},
+            include : {},
+            orderBy : {},
+            skip : 0,
+            take : 10,
+        };
+
+        this.countQuery ={
+            where : {},
+        }
+    }
+
+    search() : this {
+        const {searchTerm} = this.queryParams;
+        const { searchableFields} = this.config;
+        // doctorSearchableFields = ['user.name', 'user.email', 'specialties.specialty.title' , 'specialties.specialty.description']
+        if(searchTerm && searchableFields && searchableFields.length > 0){
+            const searchConditions : Record<string, unknown>[] = searchableFields.map((field) => {
+                if(field.includes(".")){
+                    const parts = field.split(".");
+
+                    if(parts.length === 2){
+                        const [relation, nestedField] = parts;
+
+                        const stringFilter : PrismaStringFilter = {
+                            contains : searchTerm,
+                            mode : 'insensitive' as const,
+                        }
+
+                        return {
+                            [relation] : {
+                                [nestedField] : stringFilter
+                            }
+                        }
+                    }else if(parts.length === 3){
+                        const [relation, nestedRelation, nestedField] = parts;
+
+                        const stringFilter : PrismaStringFilter = {
+                            contains : searchTerm,
+                            mode : 'insensitive' as const,
+                        }
+
+                        return {
+                            [relation] : {
+                                some :{
+                                    [nestedRelation]: {
+                                        [nestedField]: stringFilter
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    
+                }
+                // direct field
+                const stringFilter: PrismaStringFilter = {
+                    contains: searchTerm,
+                    mode: 'insensitive' as const,
+                }
+
+                return {
+                    [field]: stringFilter
+                }
+            }
+        )
+
+        const whereConditions = this.query.where as PrismaWhereConditions
+
+        whereConditions.OR = searchConditions;
+
+        const countWhereConditions = this.countQuery.where as PrismaWhereConditions;
+        countWhereConditions.OR = searchConditions;
+        }
+
+        return this;
+    }
+     
 
 }
