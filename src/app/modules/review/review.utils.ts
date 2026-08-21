c8e1047a44bf9423017a39fc190ba55545028a6d
rifat <rifat999por@gmail.com>
2026-08-21 23:48:07 +0600

    added review route

---
+++ src/app/modules/review/review.utils.ts
@@ -0,0 +1,12 @@
+
+export interface ICreateReviewPayload{
+    appointmentId:string,
+    rating:number,
+    comment:string
+}
+
+
+export interface IUpdateReviewPayload{
+    rating:number,
+    comment:string
+}
