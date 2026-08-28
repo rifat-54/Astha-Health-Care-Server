f4a5f30fe84c1e90864a2f607ae50678db62fb25
rifat <rifat999por@gmail.com>
2026-08-29 01:32:50 +0600

    create pdf

--- src/app/modules/prescription/prescription.utils.ts
+++ src/app/modules/prescription/prescription.utils.ts
@@ -1,3 +1,33 @@
-const updatePrescioption =()=>{
-    console.log("update prescription ")
+import PDFDocument from "pdfkit"
+
+interface IPrescriptionData{
+    doctorName:string,
+    doctorEmail:string,
+    patientName: string;
+    patientEmail: string;
+    followUpDate: Date;
+    instructions: string;
+    prescriptionId : string;
+    appointmentDate: Date;
+    createdAt: Date;
+}
+
+
+export const generatePrescriptionPDF=async(prescriptionData:IPrescriptionData):Promise<Buffer> =>{
+    return new Promise((resolve,reject)=>{
+
+        try {
+            const doc=new PDFDocument({
+                size:"a4",
+                margin:"50"
+            })
+
+            
+
+
+        } catch (error) {
+            
+        }
+
+    })
 }
